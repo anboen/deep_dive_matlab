73af3fee34611841de021522ee747fe24cb46680
André Böni <115255991+anboen@users.noreply.github.com>
2023-01-17 10:21:35 +0100

    Update calcBMI.m
    bug fix

--- bmi/calcBMI.m
+++ bmi/calcBMI.m
@@ -1,6 +1,7 @@
 function bmi = calcBMI(height, weight)
-    if (validateWeight(weight) || validateHeight(height))
-        throw error("weight or height not valide") 
-    end 
-    bmi = round(weight / height^2, 2);
+    if (validateWeight(weight) & validateHeight(height))
+        bmi = round(weight / height^2, 2);
+    else 
+        error("weight or height not valide") 
+    end
 end
